6b5cc4c6403d09ad95dd8445f3086a3c604f7f9a
Sallai András <andrewteki@gmail.com>
2023-05-13 00:34:31 +0200

    A beforeEach beszúrása

--- test/test.spec.js
+++ test/test.spec.js
@@ -10,15 +10,17 @@ describe('Üzenő', () => {
     beforeAll(() => {
         driver = new Builder().forBrowser("chrome").build();
     });
-    it('Egy', async () => {
+    beforeEach(async () => {
         await driver.get(url);
+    });
+    it('Egy', async () => {
+       
         await driver.findElement(By.id('msg')).sendKeys('Valami');        
         await driver.findElement(By.id('enterButton')).click();
         const text = await driver.findElement(By.className('box')).getText();        
         expect(text).toBe('Valami');
     });
     it('Kettő', async () => {
-        await driver.get(url);
         await driver.findElement(By.id('msg')).sendKeys('Más');        
         await driver.findElement(By.id('enterButton')).click();
         const text = await driver.findElement(By.className('box')).getText();        
